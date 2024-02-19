a1f04162f11736173359698cd1c12385dd7e9ba3
BalDiana <dianabalanuta333@gmail.com>
2024-02-19 10:32:30 -0500

    create RemoveDuplicates.java

---
+++ src/week4_diana/RemoveDuplicates.java
@@ -0,0 +1,19 @@
+package week4_diana;
+
+public class RemoveDuplicates {
+
+    public static String removeDuplicates(String str){
+        String result = "";
+
+        for(int i = 0; i < str.length()-1; i++){
+            char each = str.charAt(i);
+
+            if(!result.contains("" + each)){
+                result += each;
+            }
+        }
+        return result;
+
+    }
+
+}
