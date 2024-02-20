64d8cbd21e8b9076269957a2b94df096ce2ee608
Gulnar825 <gulnar.ahat825@gmail.com>
2024-02-19 21:16:06 -0500

    create OddOrEven.java

---
+++ src/week1_gulnar/OddOrEven.java
@@ -0,0 +1,22 @@
+package week1_gulnar;
+
+public class OddOrEven {
+
+
+        public static String identify(int number) {
+            if (number % 2 == 0) {
+                return "Even";
+            } else {
+                return "Odd";
+            }
+        }
+
+        public static void main(String[] args) {
+            int num1 = 5;
+            int num2 = 6;
+
+            System.out.println(num1 + " is " + identify(num1));
+            System.out.println(num2 + " is " + identify(num2));
+        }
+    }
+
