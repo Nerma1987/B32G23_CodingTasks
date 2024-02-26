ed369e0c64a761f52d99aa2f45d0bd8ccd3ba610
Gulnar825 <gulnar.ahat825@gmail.com>
2024-02-25 23:25:21 -0500

    create uniqueLetters.java

---
+++ src/week5/UniqueLetters.java
@@ -0,0 +1,35 @@
+package week5;
+
+import java.util.*;
+
+public class UniqueLetters {
+
+    public static void main(String[] args) {
+
+        String str = "AAABBBCCCDEF";
+        System.out.println(unique(str));
+
+
+    }
+    public static String unique(String str){
+        String result ="";
+        for (int i = 0; i < str.length(); i++) {
+          char ch = str.charAt(i); //returns the character in specific place in the string, so assigning them one by one into ch
+            if (str.indexOf(ch) ==str.lastIndexOf(ch)){ //ch showed up for the first time as well as fot the last time in the loop
+                result +=ch;
+
+            }
+        }
+
+        return result;
+    }
+
+
+}
+
+
+
+/*
+String -- Find the unique
+Write a return method that can find the unique characters from the String Ex: unique("AAABBBCCCDEF") ==> "DEF";
+ */
